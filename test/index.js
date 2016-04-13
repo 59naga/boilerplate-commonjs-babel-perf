b9714b647dd0cde58e29e18792bee63f5897d41e
59naga <i59naga@icloud.com>
2016-04-13 21:44:42 +0900

    chore: tweaks
    - add npm-statement/babel-plugin-transform-object-assign
    - remove cross-env
    - tweaks the scripts
    - bump eslint-config-airbnb@7.0.0

--- test/index.js
+++ test/index.js
@@ -1,6 +1,10 @@
-import yourModuleName from '../lib';
+// dependencies
 import assert from 'power-assert';
 
+// target
+import yourModuleName from '../lib';
+
+// specs
 describe('your-module-name', () => {
   it('spec1', () => {
     assert(yourModuleName() === 'bar');
